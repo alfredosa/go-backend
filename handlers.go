0c78c951bb2ff2d1f3a78a40cb3d7b5345e547c2
alfredosa <asuarezaceves@gmail.com>
2023-11-24 12:30:21 +0100

    Refactor file server and add API routes

--- handlers.go
+++ handlers.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type apiConfig struct {
@@ -19,8 +23,20 @@ func (a *apiConfig) incrementFileServerHitCount(next http.Handler) http.Handler 
 }
 
 func (a *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", a.fileServerHitCount)))
+	log.Printf("HITS: %d", a.fileServerHitCount)
+
+	html := `
+    <html>
+    <body>
+        <h1>Welcome, Chirpy Admin</h1>
+        <p>Chirpy has been visited %d times!</p>
+    </body>
+    </html>
+    `
+	w.Write([]byte(fmt.Sprintf(html, a.fileServerHitCount)))
+
 }
 
 func (a *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,4 +49,46 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem, api_config *apiConfig) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", http.StatusMovedPermanently).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := api_config.incrementFileServerHitCount(http.StripPrefix(pathPrefix, http.FileServer(root)))
+		fs.ServeHTTP(w, r)
+	})
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type validatePost struct {
+		Body string
+	}
+
+	var response validatePost
+	err := json.NewDecoder(r.Body).Decode(&response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if len(response.Body) > 140 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`"valid":true`))
+
 }
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -15,7 +14,9 @@ func main() {
 	const port string = ":8000"
 	const staticDir string = "./static/"
 
-	api_config := apiConfig{fileServerHitCount: 0}
+	api_config := apiConfig{
+		fileServerHitCount: 0,
+	}
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
@@ -24,32 +25,19 @@ func main() {
 
 	workDir, _ := os.Getwd()
 	filesDir := http.Dir(filepath.Join(workDir, staticDir))
-	FileServer(r, "/app", filesDir, api_config)
+	FileServer(r, "/app", filesDir, &api_config)
 
-	r.Get("/healthz", http.HandlerFunc(healthHandler))
-	r.Get("/metrics", http.HandlerFunc(api_config.metricsHandler))
-	r.Get("/reset", http.HandlerFunc(api_config.resetHandler))
+	apiRouter := chi.NewRouter()
+	apiRouter.Get("/healthz", healthHandler)
+	apiRouter.Get("/reset", api_config.resetHandler)
+	apiRouter.Post("/validate_chirp", validateChirpHandler)
+	r.Mount("/api", apiRouter)
+
+	adminRouter := chi.NewRouter()
+	adminRouter.Get("/metrics", api_config.metricsHandler)
+	r.Mount("/admin", adminRouter)
 
 	log.Printf("Serving on Port: %s\n", port)
 	http.ListenAndServe(port, r)
 
-}
-
-func FileServer(r chi.Router, path string, root http.FileSystem, api_config apiConfig) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
-	}
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", http.StatusMovedPermanently).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-	log.Printf("ADDING CONFIG GET")
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := api_config.incrementFileServerHitCount(http.StripPrefix(pathPrefix, http.FileServer(root)))
-		fs.ServeHTTP(w, r)
-	})
 }
